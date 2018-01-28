4d5fc8f1406f482b709d514ef28e72edeff196a8
TugceBaydar <tugcebaydar06@gmail.com>
2018-01-28 15:31:50 -0800

    updatessssss

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
 function myFunction() {
     var x = document.getElementById("navbar");
-    if (x.className === "classnav") {
+    if (x.className === "classnav col-md-8") {
         x.className += " responsive";
     } else {
-        x.className = "classnav";
+        x.className = "classnav col-md-8";
     }
 }
+
+// Preloader
+$(window).on('load', function () {
+    "use strict";
+
+    $('.preloader').fadeOut('slow');
+
+});
+
+$(document).ready(function () {
+    "use strict";
+          var body = $('body, html'),
+          toTop = $('.to_top')
+
+
+
+
+// to top button (1- on click)
+toTop.click(function () {
+    body.animate({scrollTop : 0}, 500);
+});
+
+  });
